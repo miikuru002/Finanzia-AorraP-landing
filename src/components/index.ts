e81b55e492fb07bf36e131a651e7e3b188845a4f
Jamutaq <jamutaqortegavelez@gmail.com>
2023-06-06 15:34:43 -0500

    fix: coflictos solucionados, integración de componentes

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,6 @@
 export { default as Navbar } from "./Navbar";
+export { default as Footer } from "./Footer";
+export { default as Conocenos } from "./Conocenos";
+export { default as PlanesSection } from "./PlanesSection";
+export { default as ContactSection } from "./ContactSection";
+export { default as SupportSection } from "./SupportSection";
